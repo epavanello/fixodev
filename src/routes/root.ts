a4033b1fad8e9ba167ce255f44500e9592033708
EmaDev <pavanello.emanuele@gmail.com>
2025-05-16 11:20:12 +0200

    no log

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,15 +1,12 @@
-import { logger } from '@/config/logger';
 import { Hono } from 'hono';
 
 const router = new Hono();
 
 router.get('/health', c => {
-  logger.info('Health route hit');
   return c.json({ status: 'ok' });
 });
 
 router.get('/', c => {
-  logger.info('Root route hit');
   return c.json({ status: 'ok' });
 });
 
