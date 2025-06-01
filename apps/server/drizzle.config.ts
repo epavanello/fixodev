4fdf1a3a3f1ee2a98d94e4eaf756d839f4d27fe9
EmaDev <pavanello.emanuele@gmail.com>
2025-06-01 18:14:09 +0200

    fix

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,10 +1,29 @@
 import type { Config } from 'drizzle-kit';
+import { config } from 'dotenv';
+import path from 'path';
+import { z } from 'zod';
+config({ path: path.resolve(process.cwd(), '.env') });
+config({ path: path.resolve(process.cwd(), '../../.env') });
 
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  DATABASE_AUTH_TOKEN: z.string(),
+});
+
+let envConfig: z.infer<typeof envSchema>;
+try {
+  envConfig = envSchema.parse(process.env);
+} catch (error) {
+  if (error instanceof z.ZodError) {
+    throw new Error(error.message);
+  }
+  throw error;
+}
 export default {
   schema: './src/db/schema.ts',
   dialect: 'turso',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
-    authToken: process.env.DATABASE_AUTH_TOKEN!,
+    url: envConfig.DATABASE_URL,
+    authToken: envConfig.DATABASE_AUTH_TOKEN,
   },
 } satisfies Config;
