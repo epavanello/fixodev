e4424c3fd478d39d689a100e752a845d72527096
EmaDev <pavanello.emanuele@gmail.com>
2025-06-01 18:10:14 +0200

    fix

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from 'drizzle-kit';
-import { envConfig } from './src/config/env';
 
 export default {
   schema: './src/db/schema.ts',
   dialect: 'turso',
   dbCredentials: {
-    url: envConfig.DATABASE_URL,
-    authToken: envConfig.DATABASE_AUTH_TOKEN,
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
   },
 } satisfies Config;
